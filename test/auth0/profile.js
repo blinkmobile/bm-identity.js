89ff3365a6988ea333e5fb1b446790bb13b0230d
Matt Carroll <mattcarroll@Matts-MacBook-Pro.local>
2016-09-13 13:49:55 +1000

    AUTH-8 # Added sliding sessions for access tokens

--- test/auth0/profile.js
+++ test/auth0/profile.js
@@ -4,11 +4,13 @@ const test = require('ava');
 const proxyquire = require('proxyquire');
 
 const requestMock = require('../helpers/request.js');
+const auth0ClientFactoryMock = require('../helpers/auth0-client-factory.js');
 
 const TEST_SUBJECT = '../../lib/auth0/profile.js';
 
 const JWT = 'a valid jwt';
 const CLIENT_NAME = 'valid client name';
+const CLIENT_ID = 'valid client id';
 const PROFILE = {
   name: 'FirstName LastName',
   awsRoles: {
@@ -21,14 +23,24 @@ test.beforeEach((t) => {
     return Promise.resolve(JWT);
   };
 
+  t.context.verifyJWT = (jwt, clientId) => {
+    return Promise.resolve(jwt);
+  };
+
   t.context.request = requestMock((url, data, callback) => {
     callback(null, {}, PROFILE);
+  });
+
+  t.context.auth0ClientFactory = auth0ClientFactoryMock(() => {
+    return Promise.resolve(CLIENT_ID);
   });
 });
 
 test.cb('getByClient() should call getJwt() to get access token with clientName', (t) => {
   const profile = proxyquire(TEST_SUBJECT, {
     'request': t.context.request,
+    './client-factory.js': t.context.auth0ClientFactory,
+    './verify-jwt.js': t.context.verifyJWT,
     '../utils/get-jwt.js': (clientName) => {
       t.is(clientName, CLIENT_NAME);
       t.end();
@@ -43,9 +55,50 @@ test.cb('getByClient() should call getJwt() to get access token with clientName'
     });
 });
 
+test.cb('getByClient() should call getClientIdByName() to get client id with clientName', (t) => {
+  const profile = proxyquire(TEST_SUBJECT, {
+    'request': t.context.request,
+    './client-factory.js': auth0ClientFactoryMock((clientName) => {
+      t.is(clientName, CLIENT_NAME);
+      t.end();
+      return Promise.resolve(CLIENT_ID);
+    }),
+    './verify-jwt.js': t.context.verifyJWT,
+    '../utils/get-jwt.js': t.context.getJWT
+  });
+
+  profile.getByClient(CLIENT_NAME)
+    .catch((error) => {
+      t.fail(error);
+      t.end();
+    });
+});
+
+test.cb('getByClient() should call verifyJwt() to verify jwt', (t) => {
+  const profile = proxyquire(TEST_SUBJECT, {
+    'request': t.context.request,
+    './client-factory.js': t.context.auth0ClientFactory,
+    './verify-jwt.js': (jwt, clientId) => {
+      t.is(jwt, JWT);
+      t.is(clientId, CLIENT_ID);
+      t.end();
+      return Promise.resolve(jwt);
+    },
+    '../utils/get-jwt.js': t.context.getJWT
+  });
+
+  profile.getByClient(CLIENT_NAME)
+    .catch((error) => {
+      t.fail(error);
+      t.end();
+    });
+});
+
 test.cb('getByClient() should return valid profile', (t) => {
   const profile = proxyquire(TEST_SUBJECT, {
     'request': t.context.request,
+    './client-factory.js': t.context.auth0ClientFactory,
+    './verify-jwt.js': t.context.verifyJWT,
     '../utils/get-jwt.js': t.context.getJWT
   });
 
@@ -144,7 +197,7 @@ test.cb('getByJWT() should reject if a request returns \'Unauthorized\'', (t) =>
       t.end();
     })
     .catch((error) => {
-      t.is('Unauthorised, your access token may have expired. Please use the login command to login again.', error);
+      t.is('Unauthorised, your access token may have expired. Please login again.', error);
       t.end();
     });
 });
