5bf8a73f1f66076360993b1cd99b587d94b646eb
mattcarroll139 <matt.carroll139@gmail.com>
2017-03-19 23:26:12 +1100

    Added flow-type annotations

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* @flow */
 'use strict'
 
 const assumeRole = require('./lib/aws/assume-role.js')
@@ -18,7 +19,9 @@ class BlinkMobileIdentity {
    * Create a Blink Mobile identity.
    * @param {String} clientName - The name of the client.
    */
-  constructor (clientName) {
+  constructor (
+    clientName /* : string */
+  ) {
     privateVars.set(this, {
       clientName
     })
@@ -29,15 +32,17 @@ class BlinkMobileIdentity {
    * @param {Object} options - The login options.
    * @returns {String} The JWT generated after a successful login.
    */
-  login (options) {
-    return loginCommon.login(privateVars.get(this).clientName, options)
+  login (
+    options /* : LoginOptions */
+  ) /* : Promise<string> */ {
+    return loginCommon.login((privateVars.get(this) || {}).clientName || '', options)
   }
 
   /**
    * Logout of the client.
    */
-  logout () {
-    return logoutCommon.logout(privateVars.get(this).clientName)
+  logout () /* : Promise<void> */ {
+    return logoutCommon.logout((privateVars.get(this) || {}).clientName)
   }
 
   /**
@@ -45,8 +50,10 @@ class BlinkMobileIdentity {
    * @param {Object} Additional parameters to pass to the delegation endpoint.
    * @returns {Object} The AWS credentials.
    */
-  assumeAWSRole (additionalParameters) {
-    return assumeRole(privateVars.get(this).clientName, additionalParameters)
+  assumeAWSRole (
+    additionalParameters /* : Object */
+  ) /* : Promise<Object> */ {
+    return assumeRole((privateVars.get(this) || {}).clientName, additionalParameters)
   }
 
   /**
@@ -54,11 +61,13 @@ class BlinkMobileIdentity {
    * @param {String} [accessToken] - The access token generated after a successful login. If not passed, will attempt to get the access token from the file system.
    * @returns {Object} The Auth0 profile.
    */
-  getProfile (accessToken) {
+  getProfile (
+    accessToken /* : string | void */
+  ) /* : Promise<Auth0Profile> */ {
     if (accessToken) {
       return profile.getByJWT(accessToken)
     } else {
-      return profile.getByClient(privateVars.get(this).clientName)
+      return profile.getByClient((privateVars.get(this) || {}).clientName)
     }
   }
 
@@ -66,7 +75,7 @@ class BlinkMobileIdentity {
    * Get access token generated after a successful login
    * @returns {String} The access token generated after a successful login.
    */
-  getAccessToken () {
+  getAccessToken () /* : Promise<string> */ {
     return getJWT()
   }
 
@@ -75,15 +84,17 @@ class BlinkMobileIdentity {
    * @param {Object} additionalParameters - Additional parameters to pass to the settings endpoint.
    * @returns {Object} The settings.
    */
-  getServiceSettings (additionalParameters) {
-    return settings(privateVars.get(this).clientName, additionalParameters)
+  getServiceSettings (
+    additionalParameters /* : Object */
+  ) /* : Promise<Object> */ {
+    return settings((privateVars.get(this) || {}).clientName, additionalParameters)
   }
 
   /**
    * Show the currently set and available tenants.
    * @param {String} clientName - The name of a Client.
    */
-  getTenants () {
+  getTenants () /* : Promise<Tenants> */ {
     return tenant.get()
   }
 
@@ -91,7 +102,9 @@ class BlinkMobileIdentity {
    * Change the currently set tenant, will then show the currently set and available tenants.
    * @param {String} tenantName - The name of a tenant to set.
    */
-  setTenant (tenantName) {
+  setTenant (
+    tenantName /* : string */
+  ) /* : Promise<Tenants> */ {
     return tenant.set(tenantName)
   }
 
@@ -99,9 +112,57 @@ class BlinkMobileIdentity {
    * Remove a tenant from the available tenants, will then show the currently set and available tenants.
    * @param {String} tenantName - The name of a tenant to remove.
    */
-  removeTenant (tenantName) {
+  removeTenant (
+    tenantName /* : string */
+  ) /* : Promise<Tenants> */ {
     return tenant.remove(tenantName)
   }
 }
 
 module.exports = BlinkMobileIdentity
+
+/* ::
+export type Auth0Identity = {
+  user_id: string,
+  provider: string,
+  connection: string,
+  isSocial: boolean
+}
+
+export type Auth0Profile = {
+  email_verified: boolean,
+  email: string,
+  clientID: string,
+  updated_at: string, // date
+  name: string,
+  picture: string, // url
+  user_id: string,
+  nickname: string,
+  identities: Auth0Identity[],
+  created_at: string, // date
+  global_client_id: string
+}
+
+export type AWSCredentials = {
+  accessKeyId : string,
+  secretAccessKey : string,
+  sessionToken : string
+}
+
+export type LoginOptions = {
+  email?: string | true,
+  password?: string,
+  sms?: string | true,
+  username?: string | true
+}
+
+export type Tenants = {
+  current: string | void,
+  previous: string[]
+}
+
+export type UserConfigStore = {
+  load: () => Promise<Object>,
+  update: (Object) => Promise<Object>
+}
+*/
