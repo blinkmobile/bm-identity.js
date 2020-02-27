29d674e801d565e84071f286bbae5d364f28b537
Matt Carroll <matt.carroll139@gmail.com>
2020-02-27 16:54:18 +1100

    prettier

--- index.js
+++ index.js
@@ -17,16 +17,14 @@ class BlinkMobileIdentity {
    * @param {Object} options - The login options.
    * @returns {String} The JWT generated after a successful login.
    */
-  async login (
-    options /* : ?LoginOptions */
-  ) /* : Promise<string> */ {
+  async login(options /* : ?LoginOptions */) /* : Promise<string> */ {
     return login(options)
   }
 
   /**
    * Logout of the client.
    */
-  async logout () /* : Promise<void> */ {
+  async logout() /* : Promise<void> */ {
     return logout()
   }
 
@@ -34,22 +32,25 @@ class BlinkMobileIdentity {
    * Get access token generated after a successful login
    * @returns {String} The access token generated after a successful login.
    */
-  async getAccessToken () /* : Promise<string> */ {
+  async getAccessToken() /* : Promise<string> */ {
     if (process.env.BLINKM_ACCESS_KEY && process.env.BLINKM_SECRET_KEY) {
       const expiryInMS = Date.now() + 1000 * 60 * 15 // expires in 15 minutes
-      return Promise.resolve(jwt.sign({
-        iss: process.env.BLINKM_ACCESS_KEY,
-        exp: Math.floor(expiryInMS / 1000) // exp claim should be in seconds, not milliseconds
-      }, process.env.BLINKM_SECRET_KEY))
+      return Promise.resolve(
+        jwt.sign(
+          {
+            iss: process.env.BLINKM_ACCESS_KEY,
+            exp: Math.floor(expiryInMS / 1000), // exp claim should be in seconds, not milliseconds
+          },
+          process.env.BLINKM_SECRET_KEY,
+        ),
+      )
     }
     const token = await getJWT()
     return verifyJWT(token)
   }
 
-  async getPayload (
-    accessToken /* : string | void */
-  ) /* : Promise<Object> */ {
-    const token = accessToken || await this.getAccessToken()
+  async getPayload(accessToken /* : string | void */) /* : Promise<Object> */ {
+    const token = accessToken || (await this.getAccessToken())
     return jwt.decode(token)
   }
 }
